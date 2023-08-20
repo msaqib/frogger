2cb36d6f2fc73ec7f3f1b4706b91adaeddede4b7
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-08-20 12:35:43 +0500

    Applied grass, water, and road classes in code. Added the frog, car, and truck classes. Setup the initial screen. Added a row of cars near the bottom

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-console.log('Hello world')
+const rows = 10
+const columns = 41
+
+const container = document.getElementById('container')
+
+let frogLocation = (rows - 1) * columns + Math.floor(columns / 2)
+
+console.log(frogLocation)
+
+function initialLayout() {
+    for (let i = 0 ; i < rows ; i++) {
+        for (let j = 0 ; j < columns ; j++) {
+            const elem = document.createElement('div')
+            container.appendChild(elem)
+        }
+    }
+
+    // Make the top row grass
+    setUpRows(0, 0, 'grass')    
+
+    // Make the bottom row grass
+    setUpRows(9, 9, 'grass')    
+
+    // Show the frog
+    const divs = container.children
+    divs[frogLocation].classList.remove('grass')
+    divs[frogLocation].classList.add('frog')
+
+    // Rows 2, 3, and 4 are water
+    setUpRows(1, 3, 'water')    
+    // Row 5 is grass
+    setUpRows(4, 4, 'grass')        
+    // Rows 6, 7, 8, and 9 are road
+    setUpRows(5, 8, 'road')
+}
+
+function setUpRows(startingRow, endingRow, style) {
+    const divs = container.children
+    for (let i = startingRow ; i <= endingRow ; i++) {
+        for (let j = 0 ; j < columns ; j++) {
+            divs[i*columns + j].classList.add(style)
+        }
+    }
+}
+
+function showCarRow1() {
+    const divs = container.children
+
+    let car1Rear = 8*columns
+    let car2Rear = car1Rear + 5
+
+    for (let i = 0 ; i < 3 ; i++) {
+        divs[car1Rear].classList.remove('road')
+        divs[car1Rear + 1].classList.remove('road')
+        divs[car1Rear + 1].classList.add('car-front')
+        divs[car1Rear].classList.add('car-back')
+
+        divs[car2Rear].classList.remove('road')
+        divs[car2Rear + 1].classList.remove('road')
+        divs[car2Rear + 1].classList.add('car-front')
+        divs[car2Rear].classList.add('car-back')
+
+        car1Rear += 15
+        car2Rear = car1Rear + 5
+    }
+
+    
+}
+
+initialLayout()
+showCarRow1()
